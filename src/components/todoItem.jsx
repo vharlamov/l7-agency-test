35eab3cac4a5493689b65100e518e4a8d57568de
Valeriy Harlamov <valera545@gmail.com>
2022-04-28 11:18:57 +0300

    fixed

--- src/components/todoItem.jsx
+++ src/components/todoItem.jsx
@@ -2,10 +2,7 @@ import React from 'react'
 
 const TodoItem = ({ todo, handleClick }) => {
 	return (
-		<div
-			className='card mb-3 p-3 d-flex flex-row justify-content-between'
-			id={todo.id}
-		>
+		<div className='card mb-3 p-3 d-flex flex-row justify-content-between'>
 			<p>{todo.value}</p>
 			<button className='btn btn-danger' onClick={() => handleClick(todo.id)}>
 				Сделано!
